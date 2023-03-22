75334bb0bf7c8f198c519c12dd4a43b670ce5aa5
imn20 <roob8998@gmail.com>
2023-03-22 19:49:02 +0000

    fixing server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const morgan = require('morgan');
+
+morgan('tiny');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send("Hello World!");
+})
+
+app.listen(3000, ()=>{
+    console.log("Listening on port 3000");
+})
